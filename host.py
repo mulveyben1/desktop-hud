5dda76319e82c3c7da19363de763ec35dbeab6e4
Ben Mulvey <ben@mulveyfamily.com>
2018-10-22 13:34:28 -0400

    linux compatible, dynamic disks
    completely fucked up the commit before

--- host.py
+++ host.py
@@ -3,16 +3,15 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 import time
 
 hostName = ""
-hostPort = 80
+hostPort = 8080
 p = psutil.Process()
 with p.oneshot():
     cpu_count = psutil.cpu_count(logical=True)
     total_mem = psutil.virtual_memory().total / 1073741824  # mem in GB
-    c_size = psutil.disk_usage('C:\\').total / 1073741824
-    d_size = psutil.disk_usage('D:\\').total / 1073741824
-    f_size = psutil.disk_usage('F:\\').total / 1073741824
-    g_size = psutil.disk_usage('G:\\').total / 1073741824
-    i_size = psutil.disk_usage('I:\\').total / 1073741824
+    numdisks = len(list(psutil.disk_partitions()))
+    parts = {}
+    for part in psutil.disk_partitions():
+        parts['%s' % part.mountpoint] = part
 
 
 def get_sys_info():
@@ -22,17 +21,11 @@ def get_sys_info():
         time.sleep(0.1)
         buffer['cpu_%s' % i] = psutil.cpu_percent(interval=None, percpu=True)[i]
     buffer['total_mem'] = total_mem
-    buffer['free_mem'] = psutil.virtual_memory().free / 1073741824
-    buffer['c_size'] = c_size
-    buffer['d_size'] = d_size
-    buffer['f_size'] = f_size
-    buffer['g_size'] = g_size
-    buffer['i_size'] = i_size
-    buffer['c_used'] = psutil.disk_usage('C:\\').used / 1073741824
-    buffer['d_used'] = psutil.disk_usage('D:\\').used / 1073741824
-    buffer['f_used'] = psutil.disk_usage('F:\\').used / 1073741824
-    buffer['g_used'] = psutil.disk_usage('G:\\').used / 1073741824
-    buffer['i_used'] = psutil.disk_usage('I:\\').used / 1073741824
+    buffer['free_mem'] = psutil.virtual_memory().available / 1073741824
+    for part in parts.keys():
+        buffer['size_%s' % part] = psutil.disk_usage('%s' % part).total / 1073741824
+        buffer['used_%s' % part] = psutil.disk_usage('%s' % part).used / 1073741824
+    buffer['numdisks'] = numdisks
 
     return buffer
 
